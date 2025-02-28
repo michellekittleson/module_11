12c848fa93f0d39e41495f37de1bc712e2ed66bb
michellekittleson <michelle.kittleson@gmail.com>
2025-02-28 11:38:42 -0600

    second commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,14 +11,3 @@ export default HomePage;
 
 
 
-
-// function HomePage() {
-//     return (
-//         <div>
-//             <h1>Welcome to Our E-Commerce App</h1>
-//             <p>This is the place to find all your needs at one click.</p>
-//         </div>
-//     )
-// }
-
-// export default HomePage;
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -16,17 +16,3 @@ export default NotFound;
 
 
 
-
-// import { Link } from "react-router-dom";
-
-// function NotFound() {
-//     return (
-//         <div>
-//             <h2>404 - Not Found</h2>
-//             <p>Sorry, the page you are lookng for does not exist.</p>
-//             <p>You can always go back to the <Link to="/">homepage</Link>.</p>
-//         </div>
-//     )
-// }
-
-// export default NotFound;
